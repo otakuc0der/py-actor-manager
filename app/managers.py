bb7290852f69c6c60fcbc9d36077ff551261b7f6
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2025-09-29 01:36:07 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,56 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(
+            self,
+            db_name: str,
+            table_name: str
+    ) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self._connection = sqlite3.connect(self.db_name)
+
+    def create(
+            self,
+            first_name: str,
+            last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list[Actor]:
+        cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name "
+            f"FROM {self.table_name}"
+        )
+        return [
+            Actor(*entity_tuple)
+            for entity_tuple in cursor.fetchall()
+        ]
+
+    def update(
+            self,
+            pk: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+            (new_first_name, new_last_name, pk)
+        )
+        self._connection.commit()
+
+    def delete(self, pk: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            "WHERE id = ?",
+            (pk,)
+        )
+        self._connection.commit()
